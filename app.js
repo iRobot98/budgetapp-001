f1e0fde692036561a6e137c038c3b08121331f14
iRobot98 <matendejudah@gmail.com>
2023-06-17 13:40:57 +0000

    editing A.T. documentation

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const { splitUrl } = require("./src/utils/001")
 const port =  3000
 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("*",require("./src"))
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
