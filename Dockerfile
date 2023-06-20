24707e1fdd62f6248023e50777dd1f7500bdbbb9
iRobot98 <matendejudah@gmail.com>
2023-06-20 10:04:40 +0000

    working on app

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ COPY package*.json ./
 RUN npm install --legacy-peer-deps
 COPY . .
 
-EXPOSE 3000
+EXPOSE 5000
 CMD npm run start
--- app.js
+++ app.js
@@ -6,35 +6,35 @@ const favicon = require('serve-favicon')
 const path = require('path')
 const { registered_urls } = require("./settings")
 const { splitUrl } = require("./src/utils/001")
-const port =  3000
+const port = process.env.PORT ? process.env.PORT : 5000
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("*",require("./src"))
+app.use("*", require("./src"))
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 // REGISTER APP URLS
-app.get("/*",require("./src/router_import"),(req,res, callNext)=>{
-    const {originalUrl} = req
+app.get("/*", require("./src/router_import"), (req, res, callNext) => {
+    const { originalUrl } = req
     const split_ = splitUrl(originalUrl)
-    console.log(originalUrl,"\n",split_.last)
-    if(registered_urls.app.includes(split_.last) && split_.split[0] == ''){
+    console.log(originalUrl, "\n", split_.last)
+    if (registered_urls.app.includes(split_.last) && split_.split[0] == '') {
         res.type("html")
-        res.setTimeout(100*60*60*24)
+        res.setTimeout(100 * 60 * 60 * 24)
         res.send(fs.readFileSync("./views/app/build/index.html"))
         return
     }
     callNext()
 })
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.type("html")
-    res.setTimeout(100*60*60*24)
+    res.setTimeout(100 * 60 * 60 * 24)
     res.send(fs.readFileSync("./views/app/build/index.html"))
 })
 
-app.get("/",express.static("views"))
+app.get("/", express.static("views"))
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
