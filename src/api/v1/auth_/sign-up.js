8e67ccf36592b6e28430c3050d6c9d98fd0c5d90
iRobot98 <matendejudah@gmail.com>
2023-06-30 14:45:26 +0300

    working on server

--- src/api/v1/auth_/sign-up.js
+++ src/api/v1/auth_/sign-up.js
@@ -5,6 +5,7 @@ const example_request = {
         last_name: "",
     },
     user_name: "",
+    password: "",
     phone_number: [""],
     id_number: "",
     country: "",
@@ -20,6 +21,7 @@ const check_each_field = (
             last_name: "",
         },
         user_name: "",
+        password: "",
         phone_number: [""],
         id_number: "",
         country: "",
@@ -62,7 +64,7 @@ const check_each_field = (
         }
     }
 
-    if (!e_f.user_name || typeof e_f.full_name !== "string") {
+    if (!e_f.user_name || typeof e_f.user_name !== "string") {
         errors["user_name"] = {
             type: "expected, string",
         };
@@ -76,6 +78,46 @@ const check_each_field = (
         }
     }
 
+    if (!e_f.password || typeof e_f.password !== "string") {
+        errors["password"] = {
+            type: "expected, string",
+        };
+
+        if (!e_f.password) {
+            errors["password"].missing = true;
+        } else {
+            if (typeof e_f.full_name != "string") {
+                errors["password"].error = "wrong type";
+            }
+        }
+    }
+
+    if (!e_f.id_number || typeof e_f.id_number !== "string") {
+        errors["id_number"] = {
+            type: "expected, string",
+        };
+
+        if (!e_f.id_number) {
+            errors["id_number"].missing = true;
+        } else {
+            if (typeof e_f.full_name != "string") {
+                errors["id_number"].error = "wrong type";
+            }
+        }
+    }
+    if (!e_f.country || typeof e_f.country !== "string") {
+        errors["country"] = {
+            type: "expected, string",
+        };
+
+        if (!e_f.country) {
+            errors["country"].missing = true;
+        } else {
+            if (typeof e_f.full_name != "string") {
+                errors["country"].error = "wrong type";
+            }
+        }
+    }
     return errors;
 };
 module.exports = {
